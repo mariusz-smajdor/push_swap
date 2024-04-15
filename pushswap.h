bfc4968fc82c5309a14e12cc6e78b168374d2c3f
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-04-15 19:53:10 +0000

    reverse rotate instructions

--- pushswap.h
+++ pushswap.h
@@ -29,6 +29,9 @@ void	pa(t_list **stack_a, t_list **stack_b);
 void	pb(t_list **stack_a, t_list **stack_b);
 void	ra(t_list **stack_a);
 void	rb(t_list **stack_b);
+void	rra(t_list **stack_a);
+void	rrb(t_list **stack_b);
+void	rrr(t_list **stack_a, t_list **stack_b);
 
 void	validate_input(char **av, bool is_split);
 t_list	*init_stack(char **av, bool is_split);
