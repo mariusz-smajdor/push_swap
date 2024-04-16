feab3101566edd70151c8fb57c5a9f468c763d9b
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-04-16 18:02:31 +0000

    stack sorting for three elements

--- sort_stack.c
+++ sort_stack.c
@@ -1,9 +1,13 @@
 #include "pushswap.h"
 
-static void	sort_two(t_list **stack_a)
+static void sort_three(t_list **stack_a)
 {
 	if ((*stack_a)->number > (*stack_a)->next->number)
 		sa(stack_a);
+	if ((*stack_a)->number > (*stack_a)->next->next->number)
+		rra(stack_a);
+	if ((*stack_a)->next->number > (*stack_a)->next->next->number)
+		sa(&(*stack_a)->next);
 }
 
 void	sort_stack(t_list **stack_a, t_list **stack_b)
@@ -11,6 +15,8 @@ void	sort_stack(t_list **stack_a, t_list **stack_b)
 	size_t	size;
 
 	size = ft_lstsize(*stack_a);
-	if (size == 2)
-		sort_two(stack_a);
+	if (size == 2 && (*stack_a)->number > (*stack_a)->next->number)
+		sa(stack_a);
+	if (size == 3)
+		sort_three(stack_a);
 }
