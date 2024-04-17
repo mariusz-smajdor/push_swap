f1e594491c40c1d15cb85c9884ca6b49e17bf6fc
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-04-17 12:20:12 +0000

    fix wrong approach in sort_three function

--- sort_stack/small_sort.c
+++ sort_stack/small_sort.c
@@ -2,12 +2,13 @@
 
 static void	sort_three(t_list **stack_a)
 {
+	if ((*stack_a)->number > (*stack_a)->next->next->number 
+		&& (*stack_a)->number > (*stack_a)->next->number)
+		ra(stack_a);
+	if ((*stack_a)->next->number > (*stack_a)->next->next->number)
+		rra(stack_a);
 	if ((*stack_a)->number > (*stack_a)->next->number)
 		sa(stack_a);
-	if ((*stack_a)->number > (*stack_a)->next->next->number)
-		rra(stack_a);
-	if ((*stack_a)->next->number > (*stack_a)->next->next->number)
-		sa(&(*stack_a)->next);
 }
 
 void	small_sort(t_list **stack_a, t_list **stack_b, size_t size)
