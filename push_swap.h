dd8b79508c452df99f40a14dba6f89e7b5700dfd
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-04-27 21:59:02 +0000

    add ft_substr function

--- push_swap.h
+++ push_swap.h
@@ -54,6 +54,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
 size_t	ft_strlen(const char *s);
