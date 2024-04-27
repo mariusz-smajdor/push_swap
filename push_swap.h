b1e8685e251814154321d496795e33cb5df7c4be
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-04-27 21:58:05 +0000

    add ft_strjoin function

--- push_swap.h
+++ push_swap.h
@@ -52,7 +52,8 @@ char	**ft_split(const char *s, char c);
 char	*ft_get_next_line(int fd);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *s1)
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
 size_t	ft_strlen(const char *s);
