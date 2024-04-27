8c0efea2b194b64fe881d1e798480f2bbb104b5d
Mariusz Smajdor <101614883+mariusz-smajdor@users.noreply.github.com>
2024-04-27 22:11:14 +0000

    fix error handling and memory allocation bugs

--- app/push_swap/process_input.c
+++ app/push_swap/process_input.c
@@ -6,13 +6,13 @@
 /*   By: msmajdor <msmajdor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 22:04:37 by msmajdor          #+#    #+#             */
-/*   Updated: 2024/04/20 21:50:53 by msmajdor         ###   ########.fr       */
+/*   Updated: 2024/04/27 23:46:50 by msmajdor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
 
-static void	validate_number(char **av, char *v)
+static void	validate_number(char **av, char *v, bool splitted)
 {
 	size_t	i;
 
@@ -22,12 +22,17 @@ static void	validate_number(char **av, char *v)
 		if (i == 0 && ft_issign(v[i]) && ft_isdigit(v[i + 1]))
 			i++;
 		if (!ft_isdigit(v[i]))
-			ft_arrfree(av, true);
+			{
+				if (splitted)
+					ft_arrfree(av, true);
+				else
+					ft_error();
+			}
 		i++;
 	}
 }
 
-static void	validate_limits(char **av, char *v)
+static void	validate_limits(char **av, char *v, bool splitted)
 {
 	char	*limit;
 	int		max_len;
@@ -44,11 +49,12 @@ static void	validate_limits(char **av, char *v)
 		limit = "2147483647";
 	}
 	len = ft_strlen(v);
-	if (len > max_len || (len == max_len && ft_strncmp(v, limit, max_len) > 0))
+	if ((len > max_len || (len == max_len
+				&& ft_strncmp(v, limit, max_len) > 0)) && splitted)
 		ft_arrfree(av, true);
 }
 
-static void	validate_repetition(char **av)
+static void	validate_repetition(char **av, bool splitted)
 {
 	size_t	i;
 	size_t	j;
@@ -60,7 +66,12 @@ static void	validate_repetition(char **av)
 		while (av[j])
 		{
 			if (ft_atoi(av[i]) == ft_atoi(av[j]))
-				ft_arrfree(av, true);
+			{
+				if (splitted)
+					ft_arrfree(av, true);
+				else
+					ft_error();
+			}
 			j++;
 		}
 		i++;
@@ -71,23 +82,26 @@ char	**process_input(int ac, char **av)
 {
 	size_t	i;
 	char	**new_av;
+	bool	splitted;
 
+	splitted = false;
 	if (ac < 2)
 		ft_error();
 	if (ac == 2)
 	{
 		new_av = ft_split(*av, ' ');
 		av = new_av;
+		splitted = true;
 	}
 	if (av[0] == NULL)
 		ft_arrfree(av, true);
 	i = 0;
 	while (av[i])
 	{
-		validate_number(av, av[i]);
-		validate_limits(av, av[i]);
+		validate_number(av, av[i], splitted);
+		validate_limits(av, av[i], splitted);
 		i++;
 	}
-	validate_repetition(av);
+	validate_repetition(av, splitted);
 	return (av);
 }
